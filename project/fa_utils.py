35dea3f2d1fb3f0181e343bcce25163143e5b342
Ivan Azarnikov <54893829+esvault@users.noreply.github.com>
2022-09-17 15:13:21 +0300

    Added two functions

---
+++ project/fa_utils.py
@@ -0,0 +1,54 @@
+from typing import Optional, AbstractSet
+
+from pyformlang.regular_expression import Regex
+from pyformlang.finite_automaton import DeterministicFiniteAutomaton
+from pyformlang.finite_automaton import NondeterministicFiniteAutomaton
+from pyformlang.finite_automaton import EpsilonNFA
+from pyformlang.finite_automaton import State
+from networkx import Graph
+
+
+def regex_to_dfa(regex: str) -> DeterministicFiniteAutomaton:
+    """
+    Transform regular expression to minimized Deterministic finite automaton.
+
+    :param regex: original regular expression
+    :return: minimized dfa
+    """
+
+    re = Regex(regex)
+    eps_nfa: EpsilonNFA = re.to_epsilon_nfa()
+    return eps_nfa.to_deterministic().minimize()
+
+
+def graph_to_epsilon_nfa(
+    graph: Graph,
+    start_states: Optional[AbstractSet[State]] = None,
+    finale_states: Optional[AbstractSet[State]] = None,
+) -> EpsilonNFA:
+    """
+    Transform the networkx graph to the Nondeterministic finite automation.
+
+    :param graph: Original networkx graph
+    :param start_states: Nodes of graph to be start in the result automation
+    :param finale_states: Nodes of graph to be final in the result automation
+    :return: nfa
+    """
+
+    nfa = NondeterministicFiniteAutomaton.from_networkx(graph)
+
+    if start_states is not None:
+        for s in start_states:
+            nfa.add_start_state(s)
+    else:
+        for n in graph.nodes:
+            nfa.add_start_state(n)
+
+    if finale_states is not None:
+        for s in finale_states:
+            nfa.add_final_state(s)
+    else:
+        for n in graph.nodes:
+            nfa.add_final_state(n)
+
+    return nfa
