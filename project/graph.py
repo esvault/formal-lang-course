1a75bf5fcb1ba281b13602adb5cf4d975cec41f9
esvault <azarnikov01@gmail.com>
2022-10-14 21:53:00 +0300

    Added reading from csv

--- project/graph.py
+++ project/graph.py
@@ -2,10 +2,12 @@ import cfpq_data
 import networkx as nx
 from collections import namedtuple
 
+from networkx import MultiGraph
 from pydot import Dot
+import csv
 
 
-def get_graph_info(name: str):
+def get_graph_info_by_name(name: str):
     """Load a graph by name. Passes through it and add unique labels to set.
 
     :return: 3-tuple (number of nodes, number of edges, all unique edge labels)
@@ -20,6 +22,10 @@ def get_graph_info(name: str):
         print("File {name} not found".format(name=name))
         return
 
+    return get_graph_info(graph)
+
+
+def get_graph_info(graph: MultiGraph):
     s = set()
     for i in dict(graph.edges).values():
         for j in i.values():
@@ -53,3 +59,13 @@ def save_graph_to_file(graph: Dot, filename: str):
 
     """
     graph.write(filename)
+
+
+def read_graph_from_csv(filename):
+    graph = nx.MultiGraph()
+    with open(filename) as csvfile:
+        rows = csv.reader(csvfile, delimiter=' ')
+        for row in rows:
+            graph.add_edges_from([(int(row[0]), int(row[1]), {'label': row[2]})])
+
+    return graph
