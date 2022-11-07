9e8ea474af0259e3093f6b882b2d0796ad16980d
esvault <azarnikov01@gmail.com>
2022-11-07 21:10:53 +0300

    Added cyk and tests

---
+++ tests/test_cyk_contains.py
@@ -0,0 +1,40 @@
+from pyformlang.cfg import CFG
+
+from project.cyk import cyk_contains
+
+cfgs = list(
+    map(
+        lambda x: CFG.from_text(x),
+        [
+            """
+        S -> A S B S | $
+        A -> (
+        B -> )
+    """,
+            """
+        S -> A B
+        A -> a A | $
+        B -> A b
+    """,
+        ],
+    )
+)
+
+wright_strings = [
+    ["", "()", "()()", "(((((())))))((()))(())()(())"],
+    ["b", "ab", "aab", "aaaaaab"],
+]
+
+wrong_strings = [[")(", "(()", "())", "()("], ["abb", "", "aa", "aabbb"]]
+
+
+def test_cfg_contains_write_string():
+    for i, string_list in enumerate(wright_strings):
+        for string in string_list:
+            assert cyk_contains(cfgs[i], string)
+
+
+def test_cfg_not_contains_wrong_string():
+    for i, string_list in enumerate(wrong_strings):
+        for string in string_list:
+            assert not cyk_contains(cfgs[i], string)
